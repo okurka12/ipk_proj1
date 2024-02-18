45b9aa81f498342475a74eedb07afd20dfdcc402
Vit Pavlik <pavlikvitek13@gmail.com>
2024-02-18 06:50:48 +0100

    begin with implementing ipk24chat

--- server.py
+++ server.py
@@ -14,11 +14,39 @@ FAMILY = socket.AF_INET
 # https://stackoverflow.com/questions/5815675/what-is-sock-dgram-and-sock-stream
 TYPE = socket.SOCK_DGRAM
 
+MSG_TYPES = [
+    "CONFIRM",
+    "REPLY",
+    "AUTH",
+    "JOIN",
+    "MSG",
+    "ERR",
+    "BYE",
+    "unknown"
+]
+
 
 def no_lf(s: str) -> str:
     r"""replaces (CR)LF with \n"""
     o = s.replace("\r", "")
     return o.replace("\n", "\\n")
+
+
+def recv_loop(sock: socket.socket) -> None:
+
+    while True:
+        # wait for the message
+        response = sock.recv(2048)
+        message_type = int(response[0])
+        message_id = int.from_bytes(response[1:3], byteorder="big")
+        response = response[3:]
+
+        if message_type > len(MSG_TYPES) - 2:
+            message_type = len(MSG_TYPES) -1
+        print(f"TYPE: {MSG_TYPES[message_type]}")
+        print(f"ID: {message_id}")
+        print(f"'{no_lf(response.decode('utf-8'))}'")
+
 
 
 
@@ -28,11 +56,12 @@ def main():
     sock = socket.socket(FAMILY, TYPE)
     sock.bind((UDP_IP, UDP_PORT))
 
-    # wait for the message
-    response = sock.recv(2048)
-    response = response.decode("utf-8")
+    try:
+        recv_loop(sock)
+    except KeyboardInterrupt:
+        print()
 
-    print(f"received data: '{no_lf(response)}'")
+
     print("exiting...")
 
 
