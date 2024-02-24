613ac8b256f0bf4b4982c969b150e7d84f50a5f4
Vit Pavlik <pavlikvitek13@gmail.com>
2024-02-24 06:09:21 +0100

    improve server

--- server.py
+++ server.py
@@ -6,6 +6,8 @@
 
 import socket
 import time
+import pdb
+
 
 # what address to listen on
 # BIND_IP = "127.0.0.1"  # localhost (loopback only)
@@ -17,7 +19,7 @@ FAMILY = socket.AF_INET
 # https://stackoverflow.com/questions/5815675/what-is-sock-dgram-and-sock-stream
 TYPE = socket.SOCK_DGRAM
 
-MSG_TYPES = {
+MSG_TYPES: dict[str] = {
     0x00: "CONFIRM",
     0x01: "REPLY",
     0x02: "AUTH",
@@ -28,6 +30,83 @@ MSG_TYPES = {
 }
 
 MSG_INV_TYPES = {value: key for key, value in MSG_TYPES.items()}
+
+
+class Message:
+    def __init__(self, msg: bytes) -> None:
+        # parse message header
+        self.type = int(msg[0])
+        self.type = \
+            MSG_TYPES[self.type] if self.type in MSG_TYPES else "unknown"
+        self.id = int.from_bytes(msg[1:3], byteorder="big")
+
+        # message type
+        msgt = self.type
+
+        if   msgt == "CONFIRM":
+            pass
+        elif msgt == "REPLY":
+            self.result = int(msg[3])
+            self.ref_msgid = int.from_bytes(msg[4:6], byteorder="big")
+        elif msgt == "AUTH":
+            pass
+        elif msgt == "JOIN":
+            pass
+        elif msgt == "MSG":
+            self.messagecontents = str_from_bytes(3, msg)
+        elif msgt == "ERR":
+            pass
+        elif msgt == "BYE":
+            pass
+
+        # # REPLY
+        # self.result = 0
+        # self.ref_msgid = 0
+        # self.messagecontents = ""
+
+        # # AUTH
+        # self.username = ""
+        # self.displayname = ""
+        # self.secret = ""
+
+        # # JOIN
+        # self.channel_id = ""
+        # self.displayname = ""
+
+        # # MSG
+        # self.displayname = ""
+        # self.messagecontents = ""
+
+        # # ERR
+        # self.displayname = ""
+        # self.messagecontents = ""
+
+        # BYE
+        pass
+
+    def __repr__(self) -> str:
+        output = ""
+        if self.type == "MSG":
+            output += f"TYPE: {self.type}\n"
+            output += f"ID: {self.id}\n"
+            output += f"'{no_lf(self.messagecontents)}'"
+        else:
+            output += f"TYPE: {self.type}\n ID: {self.id}"
+        return output
+
+
+
+def str_from_bytes(startpos: int, b: bytes) -> str:
+    """
+    reads a null terminated string beginning at `startpos` in `b`.
+    returns the string
+    """
+    output = ""
+    i = startpos
+    while b[i] != 0 and i < len(b) - 1:
+        output += chr(b[i])
+        i += 1
+    return output
 
 
 
@@ -44,20 +123,15 @@ def recv_loop(sock: socket.socket) -> None:
         # wait for the message
         response, retaddr = sock.recvfrom(2048)
 
-        # parse message
-        msgtype = int(response[0])
-        msgtype = MSG_TYPES[msgtype] if msgtype in MSG_TYPES else "unknown"
-        msgid = int.from_bytes(response[1:3], byteorder="big")
+        msg = Message(response)
 
         # ignore CONFIRM sent by self
-        if msgtype == "CONFIRM":
+        if msg.type == "CONFIRM":
             continue
 
         # print on stdout
         print(f"MESSAGE from {retaddr[0]}:")
-        print(f"TYPE: {msgtype}")
-        print(f"ID: {msgid}")
-        print(f"'{no_lf(response[3:].decode('utf-8'))}'")
+        print(msg)
         print()
 
         # sleep for 50 ms
