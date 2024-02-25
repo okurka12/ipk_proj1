a3d8a8000ba7c5c317bce9d376016148498906ca
Vit Pavlik <pavlikvitek13@gmail.com>
2024-02-25 11:26:50 +0100

    document sniff.sh

--- sniff.sh
+++ sniff.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # sniffs on localhost and filters UDP packets on port 4567
 sudo tcpdump -X -l -i lo port 4567
+# -X        ... header and data in hex + ascci
+# -l        ... line buffered
+# -i lo     ... interface `lo`
+# port 4567 ... filter by port 4567 (src or dst) (this only filters AUTH
+#               messages from client tho)
