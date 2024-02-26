6266986799aa8a624e2925b93813e005ce1527a0
Vit Pavlik <pavlikvitek13@gmail.com>
2024-02-26 13:40:32 +0100

    add blank udp_listener module

---
+++ udp_listener.c
@@ -0,0 +1,16 @@
+/*****************
+**  Vit Pavlik  **
+**   xpavli0a   **
+**    251301    **
+**              **
+**   Created:   **
+**  2024-02-26  **
+**              **
+**    Edited:   **
+**  2024-02-26  **
+*****************/
+
+/**
+ * implementation of the UDP Listener module - see udp_listener.h
+*/
+
---
+++ udp_listener.h
@@ -0,0 +1,16 @@
+/*****************
+**  Vit Pavlik  **
+**   xpavli0a   **
+**    251301    **
+**              **
+**   Created:   **
+**  2024-02-26  **
+**              **
+**    Edited:   **
+**  2024-02-26  **
+*****************/
+
+/**
+ * UDP listener - a listener thread that prints incoming messages and
+ * confirms messages via `udp_cnfm`
+*/
