2ccbfb04659d21d136d8f1fa8bf121e4d9e11e83
Vit Pavlik <pavlikvitek13@gmail.com>
2024-02-28 08:47:51 +0100

    make sniff.sh better

--- sniff.sh
+++ sniff.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # sniffs on localhost and filters UDP packets on port 4567
-sudo tcpdump -X -l -i lo port 4567
+
+COMMAND="sudo tcpdump -X -l ip and (udp or icmp)"
+echo "$COMMAND"
+$COMMAND
 # -X        ... header and data in hex + ascci
 # -l        ... line buffered
 # -i lo     ... interface `lo`
